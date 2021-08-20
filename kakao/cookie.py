28e9903388503354acbeae76ef94701ecbe7a13a
SJang1 <git@sjang.dev>
2021-08-20 14:10:55 +0900

    급한대로 입력 수정

--- kakao/cookie.py
+++ kakao/cookie.py
@@ -74,40 +74,42 @@ def dump_cookie(value):
 # cookie 경로가 입력되지 않았을시, 쿠키 파일이 Default 경로에 있는지 확인함
 # 경로가 입력되었거나, Default 경로의 쿠키가 존재해야 global jar 함수에 cookie를 로드함.
 def load_cookie_from_chrome():
-    cookie_file = load_cookie_config()
-    if cookie_file is False:
-        return None
+    # cookie_file = load_cookie_config()
+    # if cookie_file is False:
+    #     return None
 
-    if cookie_file is None:
-        cookie_path = None
-        os_type = platform.system()
-        if os_type == "Linux":
-            # browser_cookie3 also checks beta version of google chrome's cookie file.
-            cookie_path = os.path.expanduser("~/.config/google-chrome/Default/Cookies")
-            if os.path.exists(cookie_path) is False:
-                cookie_path = os.path.expanduser("~/.config/google-chrome-beta/Default/Cookies")
-        elif os_type == "Darwin":
-            cookie_path = os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/Cookies")
-        elif os_type == "Windows":
-            cookie_path = os.path.expandvars("%LOCALAPPDATA%/Google/Chrome/User Data/Default/Cookies")
-        else:  # Jython?
-            print("지원하지 않는 환경입니다.")
-            close()
+    # if cookie_file is None:
+    #     cookie_path = None
+    #     os_type = platform.system()
+    #     if os_type == "Linux":
+    #         # browser_cookie3 also checks beta version of google chrome's cookie file.
+    #         cookie_path = os.path.expanduser("~/.config/google-chrome/Default/Cookies")
+    #         if os.path.exists(cookie_path) is False:
+    #             cookie_path = os.path.expanduser("~/.config/google-chrome-beta/Default/Cookies")
+    #     elif os_type == "Darwin":
+    #         cookie_path = os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/Cookies")
+    #     elif os_type == "Windows":
+    #         cookie_path = os.path.expandvars("%LOCALAPPDATA%/Google/Chrome/User Data/Default/Cookies")
+    #     else:  # Jython?
+    #         print("지원하지 않는 환경입니다.")
+    #         close()
 
-        if os.path.exists(cookie_path) is False:
-            print("기본 쿠키 파일 경로에 파일이 존재하지 않습니다. 아래 링크를 참조하여 쿠키 파일 경로를 지정해주세요.\n" +
-                  "https://github.com/SJang1/korea-covid-19-remaining-vaccine-macro/discussions/403")
-            close()
+    #     if os.path.exists(cookie_path) is False:
+    #         print("기본 쿠키 파일 경로에 파일이 존재하지 않습니다. 아래 링크를 참조하여 쿠키 파일 경로를 지정해주세요.\n" +
+    #               "https://github.com/SJang1/korea-covid-19-remaining-vaccine-macro/discussions/403")
+    #         close()
 
-    jar = browser_cookie3.chrome(cookie_file=cookie_file, domain_name=".kakao.com")
+    # jar = browser_cookie3.chrome(cookie_file=cookie_file, domain_name=".kakao.com")
 
-    cookie_dict = {}
+    # cookie_dict = {}
 
-    # 쿠키를 cookie.ini 에 저장한다
-    for cookie in jar:
-        if cookie.name == '_kavacto':
-            cookie_dict['_kavacto'] = cookie.value
-            dump_cookie(cookie.value)
-            break
+    # # 쿠키를 cookie.ini 에 저장한다
+    # for cookie in jar:
+    #     if cookie.name == '_kavacto':
+    #         cookie_dict['_kavacto'] = cookie.value
+    #         dump_cookie(cookie.value)
+    #         break
 
-    return cookie_dict
+    cookie = input("쿠키 값을 입력해 주세요. : \n").strip()
+    jar = {'_kavacto': cookie}
+    return jar
